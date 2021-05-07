7c15090f1ecda3db7b64e84a4665862ccc8c4bd6
manmanluoluo@gmail.com <luohuaxing@zhongan.com>
2021-05-07 16:25:43 +0800

    first commit

---
+++ src/main/java/study/piepie/algorithm/sort/BinarySearch.java
@@ -0,0 +1,16 @@
+package study.piepie.algorithm.sort;
+
+/**
+ * @author Robin
+ * @date 2021-05-07 16:23
+ **/
+public class BinarySearch {
+    public static int searh(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+
+        }
+        return -1;
+    }
+}
