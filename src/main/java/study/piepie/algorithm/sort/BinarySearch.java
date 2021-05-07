096756909070659ecb6d59a4d60cffee2e7d7c7a
manmanluoluo@gmail.com <luohuaxing@zhongan.com>
2021-05-07 17:29:01 +0800

    binary search

--- src/main/java/study/piepie/algorithm/sort/BinarySearch.java
+++ src/main/java/study/piepie/algorithm/sort/BinarySearch.java
@@ -5,12 +5,63 @@ package study.piepie.algorithm.sort;
  * @date 2021-05-07 16:23
  **/
 public class BinarySearch {
-    public static int searh(int[] nums) {
+    public static int searh(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
         while (left <= right) {
-
+            int mid = left + (right - left) / 2;
+            if (target == nums[mid]) {
+                return mid;
+            } else if (target > nums[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
         return -1;
+    }
+
+    public static int leftBound(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (target > nums[mid]) {
+                left = mid + 1;
+            } else if (target <= nums[mid]) {
+                right = mid - 1;
+            }
+        }
+        if (left == nums.length || nums[left] != target) {
+            return -1;
+        }
+        return left;
+    }
+
+    public static int rightBound(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (target >= nums[mid]) {
+                left = mid + 1;
+            } else if (target < nums[mid]) {
+                right = mid - 1;
+            }
+        }
+        if (right < 0 || nums[right] != target) {
+            return -1;
+        }
+        return right;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[] {1, 2, 2, 2, 3};
+        int index = searh(nums, 2);
+        System.out.println(index);
+        int left = leftBound(nums, 2);
+        System.out.println(left);
+        int right = rightBound(nums, 2);
+        System.out.println(right);
     }
 }
