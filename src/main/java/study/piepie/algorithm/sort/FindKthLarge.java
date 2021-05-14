723f2513a73561be18296a013d2ff83e2b996970
manmanluoluo@gmail.com <luohuaxing@zhongan.com>
2021-05-14 19:29:06 +0800

    find the kth large

---
+++ src/main/java/study/piepie/algorithm/sort/FindKthLarge.java
@@ -0,0 +1,56 @@
+package study.piepie.algorithm.sort;
+
+/**
+ * @author Robin
+ * @date 2021-05-10 15:17
+ **/
+// 找第k大
+public class FindKthLarge {
+    public static int findKthLarge(int[] nums, int k) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int p = partition(nums, low, high);
+            if (k < p) {
+                low = p + 1;
+            } else if (k > p) {
+                high = p - 1;
+            } else {
+                return nums[p];
+            }
+        }
+        return -1;
+    }
+
+    public static int partition(int[] nums, int low, int high) {
+        if (low == high) {
+            return low;
+        }
+        int i = low;
+        int j = high + 1;
+        int pivot = nums[low];
+        while (true) {
+            while (nums[++i] < pivot) {
+                if (i > high) {
+                    break;
+                }
+            }
+            while (nums[--j] < pivot) {
+                if (j < low) {
+                    break;
+                }
+            }
+            if (i == j) {
+                break;
+            }
+            swap(nums, i, j);
+        }
+        return j;
+    }
+
+    public static void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
