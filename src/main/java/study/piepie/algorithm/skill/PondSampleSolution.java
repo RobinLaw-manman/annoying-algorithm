9b4c018555e2e7babe61974b3af247de8532397f
manmanluoluo@gmail.com <luohuaxing@zhongan.com>
2021-06-25 18:13:25 +0800

    prime solution

---
+++ src/main/java/study/piepie/algorithm/skill/PondSampleSolution.java
@@ -0,0 +1,19 @@
+package study.piepie.algorithm.skill;
+
+import study.piepie.algorithm.entity.Node;
+
+import java.util.Random;
+
+/**
+ * @author Robin
+ * @date 2021-06-25 16:55
+ **/
+public class PondSampleSolution {
+    public static Node getRandom(Node head) {
+        return null;
+    }
+    public static void main(String[] args) {
+        Random random = new Random();
+        System.out.print(random.nextInt(1));
+    }
+}
