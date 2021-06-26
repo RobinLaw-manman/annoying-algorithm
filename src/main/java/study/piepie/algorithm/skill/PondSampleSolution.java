3dc670264371285d6d7f327daf9c3cb9d7dff1f4
manmanluoluo@gmail.com <luohuaxing@zhongan.com>
2021-06-26 17:35:35 +0800

    random sample

--- src/main/java/study/piepie/algorithm/skill/PondSampleSolution.java
+++ src/main/java/study/piepie/algorithm/skill/PondSampleSolution.java
@@ -9,8 +9,19 @@ import java.util.Random;
  * @date 2021-06-25 16:55
  **/
 public class PondSampleSolution {
-    public static Node getRandom(Node head) {
-        return null;
+    public static int getRandom(Node head) {
+        int res = 0;
+        int i = 0;
+        Random random = new Random();
+        Node node = head;
+        while (node != null) {
+            // random [0, i] == 0的概率为1/i
+            if (random.nextInt(++i) == 0) {
+                res = node.getValue();
+            }
+            node = node.getNext();
+        }
+        return res;
     }
     public static void main(String[] args) {
         Random random = new Random();
