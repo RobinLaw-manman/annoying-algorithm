b9c4ed7f2a7ae690ef1ae36c76919e6bc0a2b230
manmanluoluo@gmail.com <luohuaxing@zhongan.com>
2021-07-02 14:39:30 +0800

    random sample

--- src/main/java/study/piepie/algorithm/skill/PondSampleSolution.java
+++ src/main/java/study/piepie/algorithm/skill/PondSampleSolution.java
@@ -23,6 +23,30 @@ public class PondSampleSolution {
         }
         return res;
     }
+
+    public static int[] getRandom(Node head, int k) {
+        int[] res = new int[k];
+        Node node = head;
+        for (int i=0;i < k && node != null; i++) {
+            res[i] = head.getValue();
+            node = node.getNext();
+        }
+        Random random = new Random();
+        int i = k;
+        while (node != null) {
+            int j = random.nextInt(++i);
+            // j < k 的概率为k/i
+            // 第i个数以k/i的概率选择
+            // 后面的元素也是res[j]出现在j的位置，且小于k的概率为k/(i+1)*1/k. 因为有k个数，出现在j的概率为1/k
+            // 所以后续不改变这个res[j]的值的概率为1-k/(i+1)*1/k 类推
+            if (j < k) {
+                res[j] = node.getValue();
+            }
+            node = node.getNext();
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         Random random = new Random();
         System.out.print(random.nextInt(1));
