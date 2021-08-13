db010481ab4549a7ae10fc9f4a2a13b138883693
manmanluoluo@gmail.com <luohuaxing@zhongan.com>
2021-08-13 17:17:02 +0800

    hyperloglog

---
+++ src/main/java/study/piepie/algorithm/skill/PfTest.java
@@ -0,0 +1,63 @@
+package study.piepie.algorithm.skill;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * @author Robin
+ * @date 2021-08-13 17:16
+ **/
+public class PfTest {
+
+    static class BitKeeper {
+
+        private int maxbit;
+
+        public void random() {
+            long value = ThreadLocalRandom.current().nextLong(2L << 32);
+            int bit = lowZeros(value);
+            if (bit > this.maxbit) {
+                this.maxbit = bit;
+            }
+        }
+
+        private int lowZeros(long value) {
+            int i = 0;
+            for (; i < 32; i++) {
+                if (value >> i << i != value) {
+                    break;
+                }
+            }
+            return i - 1;
+        }
+    }
+
+    static class Experiment {
+
+        private int n;
+        private BitKeeper keeper;
+
+        public Experiment(int n) {
+            this.n = n;
+            this.keeper = new BitKeeper();
+        }
+
+        public void work() {
+            for (int i = 0; i < n; i++) {
+                this.keeper.random();
+            }
+        }
+
+        public void debug() {
+            System.out
+                    .printf("%d %.2f %d\n", this.n, Math.log(this.n) / Math.log(2), this.keeper.maxbit);
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 1000; i < 100000; i += 100) {
+            Experiment exp = new Experiment(i);
+            exp.work();
+            exp.debug();
+        }
+    }
+}
