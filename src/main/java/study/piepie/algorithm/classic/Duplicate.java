f941619f7052ccfbe480f50fbe07440220f9b77d
manmanluoluo@gmail.com <luohuaxing@zhongan.com>
2021-11-22 10:12:21 +0800

    find missing number

--- src/main/java/study/piepie/algorithm/classic/Duplicate.java
+++ src/main/java/study/piepie/algorithm/classic/Duplicate.java
@@ -20,8 +20,21 @@ public class Duplicate {
         return slow + 1;
     }
 
+    // 0 1 3 4
+    public static int missingNumber(int[] nums) {
+        int res = 0;
+        // 如果不缺失元素。num的大小应该为+1
+        res ^= nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            res ^= nums[i] ^ i;
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         int[] nums = new int[]{1, 2, 2, 2, 3, 4, 4, 5};
         System.out.println(removeDuplicate(nums));
+        int[] nums2 = new int[]{0, 1, 3, 4};
+        System.out.println(missingNumber(nums2));
     }
 }
