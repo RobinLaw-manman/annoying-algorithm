4695f559b9cffc1a00676640d401b43479827530
manmanluoluo@gmail.com <luohuaxing@zhongan.com>
2021-12-21 11:05:21 +0800

    binary search

--- src/main/java/study/piepie/algorithm/linklist/ListSolution.java
+++ src/main/java/study/piepie/algorithm/linklist/ListSolution.java
@@ -19,4 +19,31 @@ public class ListSolution {
         }
         return p1;
     }
+
+    public static boolean isPalindrom(Node node) {
+        return isPalindrom(node, node);
+    }
+
+    // 错误实现，应该用一个类的属性来代替tmp
+    public static boolean isPalindrom(Node tmp, Node node) {
+        if (node == null) return true;
+        boolean isPalindrom = isPalindrom(tmp, node.next);
+        boolean equal =  node.getValue() == tmp.getValue();
+        // 下面是一个错误的示范，没有充分理解，参数传递的时候，
+        // 方法调用传递引用，只是让该方法的形参赋予了实参的值，
+        // 也就是方法外面的实参变量，和方法内部的形参变量同时指向了同一块内存。
+        // 所以方法内部对形参重新赋值，只是让形参变量和外面的实参又指向了不同的地址而已
+        // 并不会改变外面实参的地址。
+        tmp = tmp.next;
+        return equal && isPalindrom;
+    }
+
+    public static void main(String[] args) {
+        Node one = new Node(1);
+        Node two = new Node(2);
+        Node three = new Node(2);
+        Node four = new Node(1);
+        one.setNext(two);two.setNext(three);three.setNext(four);
+        System.out.println(isPalindrom(one));
+    }
 }
